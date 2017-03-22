dc3b3e1716ad59a1acf34a512598496d39d4c518
Luigi Zevola <luigi.zevola@gmail.com>
2017-03-22 13:56:50 -0500

    Initial commit
    Updated example to run on nrf52840

---
+++ _build/nrf52832_xxaa_nrf_nvic.c.d
@@ -0,0 +1,104 @@
+_build/nrf52832_xxaa_nrf_nvic.c.o: \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_nvic.c \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/stdlib.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_ansi.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/newlib.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/config.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/features.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stddef.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/reent.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_ansi.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_types.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/_types.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/lock.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/cdefs.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/stdlib.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_soc.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../../../components/device/nrf.h \
+ ../../../../../../components/device/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../components/toolchain/system_nrf52.h \
+ ../../../../../../components/device/nrf52_bitfields.h \
+ ../../../../../../components/device/nrf51_to_nrf52.h \
+ ../../../../../../components/device/nrf52_name_change.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/stdlib.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/ieeefp.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_ansi.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/newlib.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_newlib_version.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/config.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/features.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stddef.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/reent.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_ansi.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_types.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/_types.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/lock.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/cdefs.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/stdlib.h:
+
+../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_soc.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/local/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_stdint.h:
+
+../../../../../../components/device/nrf.h:
+
+../../../../../../components/device/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../../components/toolchain/system_nrf52.h:
+
+../../../../../../components/device/nrf52_bitfields.h:
+
+../../../../../../components/device/nrf51_to_nrf52.h:
+
+../../../../../../components/device/nrf52_name_change.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
